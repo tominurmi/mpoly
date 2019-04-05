1fef70bd5b30df8d7b0a7964c562833919a62c1f
Tomi <tomi.nurmi@schibsted.no>
2019-04-06 00:33:19 +0200

    Here comes trouble!

---
+++ README.md
@@ -0,0 +1,24 @@
+# Mpoly
+
+Sandboxing with interactive forms.
+A small part of a larger project.
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,202 @@
+<template>
+  <section class="container">
+    <div class="wrapper">
+      <svg width="100%" height="100%" viewBox="0 0 601 501" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-miterlimit:1.5;">
+        <g transform="matrix(1,0,0,1,-949,-801)">
+            <g id="illustration" transform="matrix(0.964719,0,0,0.905742,758.61,713.22)">
+                <rect x="197.539" y="97.434" width="621.943" height="552.034" style="fill:#c9e5ce;"/>
+                <clipPath id="_clip1">
+                    <rect x="197.539" y="97.434" width="621.943" height="552.034"/>
+                </clipPath>
+                <g clip-path="url(#_clip1)">
+                    <g>
+                        <g transform="matrix(0.244111,-0.150114,0.659604,0.40562,-512.848,695.038)">
+                            <rect x="1841" y="185.098" width="348.312" height="182.902" style="fill:#9f5031;"/>
+                        </g>
+                        <g transform="matrix(1.00291,-0.616731,0.636103,0.391168,-1699.77,1578.66)">
+                            <rect x="1841" y="185.098" width="348.312" height="182.902" style="fill:#0075c2;"/>
+                        </g>
+                        <g transform="matrix(1.03657,0,0,1.10407,-786.354,-787.443)">
+                            <path d="M1462.85,1050.51l-337,194.568" style="fill:none;stroke:#333;stroke-width:8.33px;"/>
+                        </g>
+                        <g transform="matrix(1.03657,0,0,1.10407,-786.354,-787.443)">
+                            <path d="M939,1223.35l626,-361.421l-214.395,123.781" style="fill:none;stroke:#333;stroke-width:8.33px;stroke-linecap:square;"/>
+                        </g>
+                        <g transform="matrix(1.03657,0,0,1.10407,-786.354,-787.443)">
+                            <path d="M1350.61,985.707l208.568,120.417" style="fill:none;stroke:#333;stroke-width:8.33px;stroke-linecap:round;"/>
+                        </g>
+                        <g transform="matrix(1.03657,0,0,1.10407,-786.354,-787.443)">
+                            <path d="M939,1137.2l299.606,172.977" style="fill:none;stroke:#333;stroke-width:8.33px;stroke-linecap:square;"/>
+                        </g>
+                        <g transform="matrix(0.897697,-0.552034,1.03657,0.637434,-400.983,77.4607)">
+                            <text x="457.942px" y="748.801px" style="font-family:'Arial-BoldMT', 'Arial', sans-serif;font-weight:700;font-size:50px;fill:#333;">LUXURY</text>
+                            <text x="511.201px" y="800.436px" style="font-family:'Arial-BoldMT', 'Arial', sans-serif;font-weight:700;font-size:50px;fill:#333;">TAX</text>
+                        </g>
+                        <g transform="matrix(2.67433,-1.64457,0.303189,0.186444,-925.216,1171.3)">
+                            <clipPath id="_clip2">
+                                <rect x="434.478" y="673.558" width="105.135" height="167.515"/>
+                            </clipPath>
+                            <g clip-path="url(#_clip2)">
+                                <g transform="matrix(0.335671,0,0,3.41889,290.724,-1724.69)">
+                                    <text x="437.62px" y="736.87px" style="font-family:'Arial-BoldMT', 'Arial', sans-serif;font-weight:700;font-size:33.333px;fill:#333;">{{ msg }}</text>
+                                </g>
+                            </g>
+                        </g>
+                        <g transform="matrix(2.14886,-1.32143,0.8999,0.553389,-306.135,-794.4)">
+                            <rect x="-255.717" y="1650.13" width="39.457" height="65.326" style="fill:url(#_Linear3);"/>
+                        </g>
+                        <g>
+                            <g transform="matrix(0.869955,-0.534974,1.13115,0.695597,-1701.18,1003.49)">
+                                <rect x="1841" y="185.098" width="348.312" height="182.902" style="fill:#f5891c;"/>
+                            </g>
+                            <g transform="matrix(1.16506,-0.71645,1.51487,0.93156,-2386.06,1263.27)">
+                                <rect x="1841" y="185.098" width="348.312" height="182.902" style="fill:none;stroke:#333;stroke-width:2.27px;stroke-dasharray:6.82,6.82,0,0;"/>
+                            </g>
+                            <g transform="matrix(0.897697,-0.552034,1.03657,0.637434,-870.33,-238.06)">
+                                <text x="320.729px" y="935.054px" style="font-family:'Arial-BoldMT', 'Arial', sans-serif;font-weight:700;font-size:266.667px;fill:#fff;stroke:#000;stroke-width:2.68px;stroke-linejoin:round;stroke-miterlimit:1.41421;">?</text>
+                            </g>
+                        </g>
+                    </g>
+                    <g transform="matrix(1,0,0,1,42.4994,-1.10407)">
+                        <g transform="matrix(0.910065,-0.559639,0.938203,0.576943,-915.275,921.653)">
+                            <rect x="1211.59" y="220" width="84.411" height="80.233" style="fill:#009590;"/>
+                        </g>
+                        <g transform="matrix(0.903959,0.555884,-3.74834e-17,0.830472,-701.986,-485.61)">
+                            <path d="M1296,220l-7.974,-6.394l-34.301,-28.041l-42.136,34.435l0,80.233l84.411,0l0,-80.233Z" style="fill:#00c84a;"/>
+                        </g>
+                        <g transform="matrix(0.903959,-0.555884,3.74834e-17,0.830472,-625.682,908.32)">
+                            <rect x="1211.59" y="220" width="84.411" height="80.233" style="fill:#009537;"/>
+                        </g>
+                        <g transform="matrix(0.910171,-0.559704,0.476296,0.649327,-776.208,900.702)">
+                            <rect x="1211.59" y="220" width="84.411" height="80.233" style="fill:#009f3a;"/>
+                        </g>
+                        <g transform="matrix(1.03657,0,0,1.10407,-829.89,-788.547)">
+                            <path d="M1254.88,1007.21l36.054,-4.747l-74.118,42.792l-36.348,4.916l74.412,-42.961Z" style="fill:#00aa3c;"/>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>
+        <defs>
+            <linearGradient id="_Linear3" x1="0" y1="0" x2="1" y2="-0.293815" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-32.1516,-5.3717e-12,1.40687e-11,-130.651,-216.26,1650.13)"><stop offset="0" style="stop-color:#c9e5ce;stop-opacity:1"/><stop offset="1" style="stop-color:#c9e5ce;stop-opacity:0"/></linearGradient>
+        </defs>
+    </svg>
+
+    <div class="inputs">
+      <label for="address">Address</label>
+      <input id="address" v-model="msg">
+    </div>
+
+  </div>
+
+  <button>Next</button>
+
+  </section>
+</template>
+
+<script>
+//import Logo from '~/components/Logo.vue'
+
+export default {
+  data: () => ({
+    msg: '',
+  }),
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import url('https://rsms.me/inter/inter.css');
+
+*, html { font-family: 'Inter', sans-serif; }
+
+*:focus {
+outline: none;
+}
+
+body {
+transition: all 0.5s ease;
+background: #E8EDF1;
+font-size:16px;
+color: #575757;
+}
+
+.container {
+padding: 1rem;
+margin: 0 auto;
+min-height: 100vh;
+display: flex;
+justify-content: flex-start;
+align-items: center;
+flex-direction: column;
+}
+
+svg {
+  border-radius: 4px 4px 0 0;
+  height: auto;
+  width: auto;
+}
+
+.inputs {
+padding: 1rem;
+display: flex;
+flex-direction: column;
+}
+
+label {
+padding-bottom: 8px;
+font-size:16px;
+font-weight: 600;
+}
+
+input {
+-webkit-appearance: none;
+-moz-appearance: none;
+appearance: none;
+
+background: #FBFBFB;
+border: 2px solid #575757;
+box-sizing: border-box;
+border-radius: 4px;
+padding:1rem;
+font-size:16px;
+}
+
+button {
+transition: box-shadow 500ms ease;
+background: #68CFA9;
+border: 2px solid #1A332A;
+box-sizing: border-box;
+box-shadow: inset 0px -2px 0px #259374, inset 0px 2px 0px #CCFFEC;
+border-radius: 40px;
+font-size: 16px;
+font-weight: 600;
+width: 100%;
+max-width: 220px;
+padding: 1rem;
+color: #1A332A;
+margin-top: 20px;
+cursor: pointer;
+
+  &:hover {
+    box-shadow: inset 0px -3px 0px #259374, inset 0px 3px 0px #CCFFEC;
+  }
+  &:active {
+    transition: 100ms ease;
+    box-shadow: inset 0px -0px 0px #259374, inset 0px 1px 0px #CCFFEC;
+  }
+}
+
+.wrapper {
+margin-top: 30px;
+padding: 0.5rem;
+border-radius: 8px;
+background: white;
+width: 100%;
+max-width: 400px;
+box-shadow: 0px 5px 12px rgba(28, 65, 84, 0.08);
+}
+</style>
